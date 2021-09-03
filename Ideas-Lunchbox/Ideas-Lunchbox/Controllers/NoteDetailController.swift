b3bbea27753746adff46744a7b780c61336c297c
Toni Itkonen <toni.itkonen@icloud.com>
2021-09-03 20:08:15 +0300

    Project initial commit

---
+++ Ideas-Lunchbox/Ideas-Lunchbox/Controllers/NoteDetailController.swift
@@ -0,0 +1,110 @@
+import UIKit
+
+protocol NoteDelegate {
+    func saveNewNote(title: String, date: Date, text: String)
+}
+
+
+class NoteDetailController: UIViewController {
+    
+    // MARK: PROPERTIES
+    
+    let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM dd, YYYY hh:mm"
+        return dateFormatter
+    }()
+    
+    var noteData: Note! {
+        didSet {
+            textView.text = noteData.title
+            dateLabel.text = dateFormatter.string(from: noteData.date ?? Date())
+        }
+    }
+    
+    var delegate: NoteDelegate?
+    
+    fileprivate var textView: UITextView = {
+        let textField = UITextView()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.text = "Add your notes"
+        textField.isEditable = true
+        textField.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+        return textField
+    }()
+    
+    fileprivate lazy var dateLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.systemFont(ofSize: 14, weight: .light)
+        label.textColor = .gray
+        label.text = dateFormatter.string(from: Date())
+        label.textAlignment = .center
+        return label
+    }()
+    
+    
+    // MARK: LIFE CYCLE
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        setupUI()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if self.noteData == nil {
+            delegate?.saveNewNote(title: textView.text, date: Date(), text: textView.text)
+        } else {
+            // update our note here
+            guard let newText = self.textView.text else {
+                return
+            }
+            CoreDataManager.shared.saveUpdatedNote(note: self.noteData, newText: newText)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let items: [UIBarButtonItem] = [
+            UIBarButtonItem(barButtonSystemItem: .organize, target: nil, action: nil),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(barButtonSystemItem: .refresh, target: nil, action: nil),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(barButtonSystemItem: .camera, target: nil, action: nil),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+            UIBarButtonItem(barButtonSystemItem: .compose, target: nil, action: nil)
+        ]
+        
+        self.toolbarItems = items
+       
+        let topItems: [UIBarButtonItem] = [
+            UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil),
+            UIBarButtonItem(barButtonSystemItem: .action, target: nil, action: nil)
+        ]
+        
+        self.navigationItem.setRightBarButtonItems(topItems, animated: false)
+    }
+    
+    // MARK: FUNCTIONS
+    fileprivate func setupUI() {
+        view.addSubview(dateLabel)
+        view.addSubview(textView)
+        
+        dateLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 90).isActive = true
+        dateLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        dateLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        
+        textView.topAnchor.constraint(equalTo: dateLabel.bottomAnchor).isActive = true
+        textView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        textView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        textView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+    }
+    
+}
